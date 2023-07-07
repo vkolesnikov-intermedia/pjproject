fe0e39b903d8fe7298447c11ec43d9db8f0981fd
DmitriyAnd <65737456+DmitriyAnd@users.noreply.github.com>
2023-07-07 18:48:59 +0400

    Add swift file to fix pod installation (#20)

---
+++ ios_framework/Sources/LibHelper.swift
@@ -0,0 +1,12 @@
+//
+//  LibHelper.swift
+//  Pods
+//
+//  Created by Dmitriy Shamaev on 21.03.2022.
+//
+
+import Foundation
+
+final class LibHelper {}
+
+// do not delete this file or pjsip library won't be able to be linked dynamically into pure Swift project
